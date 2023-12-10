bac1b88c94d957a833b317e502da4f710847d467
Uche Mukolo <uchek7@gmail.com>
2023-12-10 20:56:45 +0000

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import AccountList from './features/goalSavings/components/accounts/AccountList.js';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <h1 style={{textAlign: 'center', paddingTop: '20px'}}>Goal Savings</h1>
+      <AccountList/>
+      
     </div>
   );
 }
-
-export default App;
